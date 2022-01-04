2e98522277f0b6a3ee288131aef58d4b48ca2187
guoliang03 <guoliang03@kuaishou.com>
2022-01-04 18:54:03 +0800

    0104. Maximum Depth of Binary Tree 迭代解法

--- leetcode/0104.Maximum-Depth-of-Binary-Tree/104. Maximum Depth of Binary Tree.go
+++ leetcode/0104.Maximum-Depth-of-Binary-Tree/104. Maximum Depth of Binary Tree.go
@@ -1,6 +1,8 @@
 package solution0104
 
-import "LearnGo/leetcode/kit"
+import (
+	"LearnGo/leetcode/kit"
+)
 
 type TreeNode = kit.TreeNode
 
@@ -12,9 +14,34 @@ func maxOf(a, b int) int {
 	}
 }
 
-func maxDepth(root *TreeNode) int {
+// 递归解法
+/*func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 	return 1 + maxOf(maxDepth(root.Left), maxDepth(root.Right))
+}*/
+
+// BFS
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	q := []*TreeNode{root}
+	depth := 0
+	for len(q) > 0 {
+		depth++
+		count := len(q)
+		for i := 0; i < count; i++ {
+			node := q[i]
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+		q = q[count:]
+	}
+	return depth
 }
