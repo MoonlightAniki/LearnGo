e66e11d981122d913aca29303877530c79170471
guoliang03 <guoliang03@kuaishou.com>
2022-01-04 18:36:23 +0800

    0589. N-ary Tree Preorder Traversal 迭代解法

--- leetcode/0589.N-ary-Tree-Preorder-Traversal/589. N-ary Tree Preorder Traversal.go
+++ leetcode/0589.N-ary-Tree-Preorder-Traversal/589. N-ary Tree Preorder Traversal.go
@@ -4,7 +4,8 @@ import "LearnGo/leetcode/kit"
 
 type Node = kit.Node
 
-func preorder(root *Node) []int {
+// 递归解法
+/*func preorder(root *Node) []int {
 	var res []int
 	if root == nil {
 		return res
@@ -12,6 +13,30 @@ func preorder(root *Node) []int {
 	res = append(res, root.Val)
 	for _, child := range root.Children {
 		res = append(res, preorder(child)...)
+	}
+	return res
+}*/
+
+// 迭代解法
+func preorder(root *Node) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+	var stack []*Node
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if len(node.Children) == 0 {
+			res = append(res, node.Val)
+		} else {
+			for i := len(node.Children) - 1; i >= 0; i-- {
+				stack = append(stack, node.Children[i])
+			}
+			node.Children = nil
+			stack = append(stack, node)
+		}
 	}
 	return res
 }
