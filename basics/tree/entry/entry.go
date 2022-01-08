49dfd9f0cfeb817a39f80d07e31b10beaeccb019
guoliang03 <guoliang03@kuaishou.com>
2022-01-08 21:39:19 +0800

    使用内嵌扩展已有类型

--- basics/tree/entry/entry.go
+++ basics/tree/entry/entry.go
@@ -5,14 +5,21 @@ import (
 	"fmt"
 )
 
-// 扩展系统类型或者别人的类型的方法有两种：
+// 扩展系统类型或者别人的类型的方法有3种：
 // 1.定义别名
 // 2.使用组合
+// 3.使用内嵌
 
 // 演示如何使用组合扩展别人的类型：
 // 假设 tree.Node 是别人定义的类型，并且只实现了中序遍历，使用组合的方式为 tree.Node 增加前序遍历和后续遍历
 type myTreeNode struct {
 	node *tree.Node
+}
+
+// 演示使用内嵌扩展已有类型
+// 内嵌看起来
+type myTreeNode2 struct {
+	*tree.Node
 }
 
 func (node *myTreeNode) postOrder() {
@@ -26,22 +33,45 @@ func (node *myTreeNode) postOrder() {
 	node.node.Print()
 }
 
+// 注意组合与内嵌的区别
+func (node *myTreeNode2) postOrder() {
+	if node == nil || node.Node == nil {
+		return
+	}
+	left := myTreeNode2{node.Left}
+	left.postOrder()
+	right := myTreeNode2{node.Right}
+	right.postOrder()
+	node.Print()
+}
+
+// 不是方法重写
+func (node *myTreeNode2) InOrderTraverse() {
+	fmt.Println("This method is shadowed.")
+}
+
 func main() {
 	//test001()
 
-	root := tree.CreateNode(1)
+	root := myTreeNode2{tree.CreateNode(1)}
 	root.Left = tree.CreateNode(2)
 	root.Right = tree.CreateNode(3)
 	root.Left.Left = tree.CreateNode(4)
 	root.Left.Right = tree.CreateNode(5)
 	root.Right.Left = tree.CreateNode(6)
 	root.Right.Right = tree.CreateNode(7)
+	fmt.Println("root.InOrderTraverse():")
 	root.InOrderTraverse()
+	fmt.Println("root.Node.InOrderTraverse():")
+	root.Node.InOrderTraverse()
 
 	fmt.Println()
 
-	myRoot := myTreeNode{root}
-	myRoot.postOrder()
+	root.postOrder()
+
+	// 内嵌看起来像是继承，但是并不是，只是组合的语法糖。
+	//var baseRoot *tree.Node
+	//baseRoot = &root
 }
 
 func test001() {
